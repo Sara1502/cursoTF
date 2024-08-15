53d01fc63628f32afaf1a6ba1f9ec3cbe85d9f43
Sara1502 <moreirasaragds@gmail.com>
2024-08-15 09:06:34 -0300

    Regreção linear

---
+++ linearRegression.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('bancos/moore.csv', on_bad_lines='skip', header=None).to_numpy()
+
+X= data[:,0].reshape(-1, 1)
+Y = data[:,1]
+
+plt.scatter(X, Y);
+plt.show()
+
+Y = np.log(Y)
+plt.scatter(X, Y);
+plt.show()
+
+X = X - X.mean()
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Input(shape=(1, )),
+    tf.keras.layers.Dense(1),
+])
+
+model.compile(
+    optimzer=tf.keras.optimizers.SGD(0.001, 0.9),
+    loss='mse',
+)
+
+def scheduler(epoch, lr):
+    if epoch >= 50:
+        return 0.0001
+    return 0.001
+
+scheduler = tf.keras.callbacks.LearningRateScheduler(scheduler)
+
+r = model.fit(X, Y, epochs=200, callbacks=[scheduler])
+
+plt.plot(r.history['loss'], label='loss')
+plt.legend()
+plt.show()
+
+model.layers
+model.layers[0].get_weights()
+
+a = model.layers[0].get_weights[0][0, 0]
+
+print(np.log(2) / a)

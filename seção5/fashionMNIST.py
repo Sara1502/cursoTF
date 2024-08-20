c4d33b25034546e01e7c48e0a3755e0f4e549ebe
Sara1502 <moreirasaragds@gmail.com>
2024-08-20 12:48:21 -0300

    Fashion MNIST

--- seção5/fashionMNIST.py
+++ seção5/fashionMNIST.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout
-from tensorflow.keras.models import Model
 from sklearn.metrics import confusion_matrix
 import itertools
 
@@ -15,17 +13,17 @@ print('x_train.shape: ', x_train.shape)
 k = len(set(y_train))
 print("numero de classes: ",k)
 
-i = Input(shape=x_train[0].shape)
-x = Conv2D(32, (3, 3), strides=2, activation='relu') (i)
-x = Conv2D(64, (3, 3), strides=2, activation='relu') (x)
-x = Conv2D(128, (3, 3), strides=2, activation='relu') (x)
-x = Flatten() (x)
-x = Dropout(0.2) (x)
-x = Dense(512, activation='relu') (x)
-x = Dropout(0.2) (x)
-x = Dense(k, activation='softmax') (x)
+i = tf.keras.layers.Input(shape=x_train[0].shape)
+x = tf.keras.layers.Conv2D(32, (3, 3), strides=2, activation='relu') (i)
+x = tf.keras.layers.Conv2D(64, (3, 3), strides=2, activation='relu') (x)
+x = tf.keras.layers.Conv2D(128, (3, 3), strides=2, activation='relu') (x)
+x = tf.keras.layers.Flatten() (x)
+x = tf.keras.layers.Dropout(0.2) (x)
+x = tf.keras.layers.Dense(512, activation='relu') (x)
+x = tf.keras.layers.Dropout(0.2) (x)
+x = tf.keras.layers.Dense(k, activation='softmax') (x)
 
-model = Model(i, x)
+model = tf.keras.models.Model(i, x)
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',

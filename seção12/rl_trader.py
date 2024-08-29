e170e7c9605b99314af0ab335b52f6fa3039e52b
Sara Moreira <104461441+Sara1502@users.noreply.github.com>
2024-08-29 09:49:51 -0300

    rl_trader.py pt3

--- seção12/rl_trader.py
+++ seção12/rl_trader.py
@@ -182,3 +182,61 @@ class MultiStockEnv:
                         self.cash_in_hand -= self.stock_price[i]
                     else:
                         can_buy = False
+
+
+
+class DQNAgent(object):
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = ReplayBuffer(state_size, action_size, size=500)
+        self.gamma = 0.95 # discout rate
+        self.epsilon = 1.0 # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.model = mlp(state_size, action_size)
+
+    
+    def update_replay_memory(self, state, action, reward, next_state, done):
+        self.memory.store(state, action, reward, next_state, done)
+
+        
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.choice(self.action_size)
+        act_value = self.model.predict(state)
+        return np.argmax(act_value[0])
+    
+
+    def replay(self, batch_size=32):
+        if self.memory.size < batch_size:
+            return
+        
+        minibatch = self.memory.sample_batch(batch_size)
+        states = minibatch['s']
+        actions = minibatch['a']
+        rewards = minibatch['r']
+        next_states = minibatch['s2']
+        done = minibatch['d']
+
+        target = rewards + self.gamma * np.amax(self.model.predict(next_states), axis=1)
+
+        target[done] = rewards[done]
+
+
+        target_full  = self.model.predict(states)
+        target_full[np.arange(batch_size), actions] = target
+
+
+        self.model.train_on_batch(states, target_full)
+        
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+
+    def load(self, name):
+            self.model.load_weights(name)
+
+        
+    def save(self, name):
+        self.model.load_weightd(name)
